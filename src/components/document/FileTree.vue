a5feae59d6e5d3d2933a7f2be03b962a2e26fae5
arczlz <zlz5v5@163.com>
2024-08-12 00:04:25 +0800

    fix(app): 🐛 文档详情

--- src/components/document/FileTree.vue
+++ src/components/document/FileTree.vue
@@ -1,7 +1,39 @@
 <template>
-  <div></div>
+  <div>
+    <q-tree
+      :nodes="simple"
+      node-key="label"
+      selected-color="primary"
+      v-model:selected="selected"
+      default-expand-all
+    />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+const selected = ref('Food');
+
+const simple = reactive([
+  {
+    label: 'Relax Hotel',
+    children: [
+      {
+        label: 'Food',
+        icon: 'restaurant_menu',
+      },
+      {
+        label: 'Room service',
+        icon: 'room_service',
+      },
+      {
+        label: 'Room view',
+        icon: 'photo',
+      },
+    ],
+  },
+]);
+</script>
 
 <style scoped></style>
